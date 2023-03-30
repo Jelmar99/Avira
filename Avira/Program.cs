ea8171d5c268afcb0fa607c75ab53e2456736d1f
Jelmar1999 <62281610+Jelmar1999@users.noreply.github.com>
2023-03-30 16:12:22 +0200

    VersionControl Adapter Implemented

--- Avira/Program.cs
+++ Avira/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Avira.Domain;
+using Avira.Domain.Adapters;
 
 Console.WriteLine("Hello, World!");
 
@@ -12,3 +13,8 @@ a.SendNotification(n);
 a.SendNotification(n);
 a.SendNotification(n);
 a.SendNotification(n);
+
+var s = new Sprint(new Guid(), new DateTime(2023, 3, 30), new DateTime(2023, 4, 13));
+var pb = new ProductBacklog(new Guid(), s);
+var p = new Project(new Guid(), pb, new GithubAdapter());
+p.Commit();
