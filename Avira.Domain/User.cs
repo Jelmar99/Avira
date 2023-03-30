4a329de5e2d7eeb519c6ba42c97b29c00f124e27
ChristianAvans <cwm.nijssen@student.avans.nl>
2023-03-30 23:10:29 +0200

    Basic builder pattern added

--- Avira.Domain/User.cs
+++ Avira.Domain/User.cs
@@ -6,15 +6,24 @@ namespace Avira.Domain;
 public class User : INotificationListener
 {
     private Guid Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; private set; }
     private Role? Role { get; set; }
     public INotificationPreference NotificationPreference { get; set; }
 
-    public User(Guid id, string name)
+    public string Email { get; private set; }
+    public string PhoneNr { get; private set; }
+    public string SlackUsername { get; private set; }
+
+    public User(Guid id, string name, Role role, INotificationPreference notificationPreference, string email,
+        string phoneNr, string slackUsername)
     {
         Id = id;
         Name = name;
-        NotificationPreference = new NotificationPreference();
+        Role = role;
+        NotificationPreference = notificationPreference;
+        Email = email;
+        PhoneNr = phoneNr;
+        SlackUsername = slackUsername;
     }
 
     public void SetRole(Role? role)
--- Avira/Program.cs
+++ Avira/Program.cs
@@ -1,16 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Avira.Domain;
+using Avira.Domain.Builder;
 using Avira.Domain.Notifications;
 
 Console.WriteLine("Hello, World!");
 
 var a = new BacklogItem(Guid.Empty, "", "", 0, 0, new Sprint(Guid.Empty, new DateTime(), new DateTime()));
-var u = new User(new Guid(), "Bob");
+INotificationPreference notif = new NotificationPreference();
+notif = new WhatsAppNotificationPreferenceDecorator(notif);
+notif = new EmailNotificationPreferenceDecorator(notif);
+notif = new SlackNotificationPreferenceDecorator(notif);
 
-u.NotificationPreference = new WhatsAppNotificationPreferenceDecorator(u.NotificationPreference);
-u.NotificationPreference = new EmailNotificationPreferenceDecorator(u.NotificationPreference);
-u.NotificationPreference = new SlackNotificationPreferenceDecorator(u.NotificationPreference);
+//var u = new User(new Guid(), "Bob");
+var u = new UserBuilder()
+    .setId(new Guid())
+    .setName("Bob")
+    .setNotificationPreference(notif)
+    .Build();
+
+
 
 var n = new Notification("test notification :)");
 a.AddListener(u);
