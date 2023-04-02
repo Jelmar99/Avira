0e3668837c102a2e060d87e49520983ffa0fa8d8
ChristianAvans <cwm.nijssen@student.avans.nl>
2023-04-02 14:01:51 +0200

    clarified adapter pattern

---
+++ Avira.Domain/VersionControl/GitLabService.cs
@@ -0,0 +1,19 @@
+namespace Avira.Domain.VersionControl;
+
+public class GitLabService
+{
+    public void CommitToGitLab()
+    {
+        Console.WriteLine("Committing to Gitlab!");
+    }
+
+    public void GitLabPush()
+    {
+        Console.WriteLine("Pushing to Gitlab!");
+    }
+
+    public void GitLabPull()
+    {
+        Console.WriteLine("Pulling from Gitlab!");
+    }
+}
